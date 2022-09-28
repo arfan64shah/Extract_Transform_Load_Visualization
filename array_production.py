6947f830ae1a72ea9e16bbdc8c6d89d607bb0983
arfan64shah <59228458+arfan64shah@users.noreply.github.com>
2022-09-28 09:28:18 +0600

    Added new changes

--- array_production.py
+++ array_production.py
@@ -1,4 +1,10 @@
 import numpy as np
 
-array = np.arange(12)
-print(array)
+# array = np.arange(12)
+# print(array)
+
+# array = np.arange(3, 10, 2)
+# print("array is ", array)
+
+zero = np.zeros(12)
+print(zero)
