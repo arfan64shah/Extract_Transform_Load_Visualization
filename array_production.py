15876e87bd403ced7b1025b061781260e410db52
arfan64shah <59228458+arfan64shah@users.noreply.github.com>
2022-09-28 10:02:05 +0600

    Added new changes

--- array_production.py
+++ array_production.py
@@ -6,5 +6,16 @@ import numpy as np
 # array = np.arange(3, 10, 2)
 # print("array is ", array)
 
-zero = np.zeros(12)
-print(zero)
+# zero = np.zeros(12)
+# print(zero)
+# line_space = np.linspace(2, 20, num=10, endpoint=True)
+# print(line_space)
+
+# array = np.array([1, 2, 3, 4, 5, 1, 2, 3, 4])
+# print(array)
+
+a = [1, 5, 3, 4, 'arfan', 7]
+print('type is ', type(a), 'a is ', a)
+
+b = np.array(a)
+print('type is ', type(b), 'b is ', b)
